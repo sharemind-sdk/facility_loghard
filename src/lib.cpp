bb2afde0eb37f37cd8999999f9b81905a8044972
Jaak Ristioja <jaak.ristioja@cyber.ee>
2017-09-07 14:53:37 +0300

    Sync with libfmodapi changes.

--- src/lib.cpp
+++ src/lib.cpp
@@ -30,7 +30,7 @@
 #include <set>
 #include <sharemind/compiler-support/GccPR44436.h>
 #include <sharemind/compiler-support/GccPR54526.h>
-#include <sharemind/libfmodapi/api_0x1.h>
+#include <sharemind/facility-module-apis/api_0x1.h>
 #include <string>
 #include <unordered_map>
 
